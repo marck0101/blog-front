b80718642e7e1a1445de955ec87350f2b11e9cfb
Marcos Henrique <marcos.henrique.a7x@gmail.com>
2026-01-14 21:46:04 -0300

    ajuste env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.blog.marck0101.com.br/",
+  baseURL: import.meta.env.VITE_ENV,
   // baseURL: "http://localhost:3333/api",
   headers: {
     "Content-Type": "application/json",
