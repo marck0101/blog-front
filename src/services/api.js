ed5257ae01e255f3b35d88d0dbeb69806bff1fb7
Marcos Henrique <marcos.henrique.a7x@gmail.com>
2026-01-29 16:55:26 -0300

    teste no front

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,20 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
   },
+});
+
+// GARANTE QUE NUNCA USA CACHE DO BROWSER
+api.interceptors.request.use((config) => {
+  config.params = {
+    ...(config.params || {}),
+    _ts: Date.now(), // cache buster
+  };
+  return config;
 });
 
 export default api;
